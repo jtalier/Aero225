fadce5433423030e449d0976e81f9043117b4654
wulframmaguire <maguired@umich.edu>
2019-04-28 23:09:56 -0400

    yuh

--- ramjet.m
+++ ramjet.m
@@ -47,7 +47,9 @@ a1 = sqrt(gamma*R*T1);
 T01 = T1/Trat;     p01 = p1/prat;   rho01 = rho1/rhorat;
 u1 = M1*a1;
 A1 = m_dot / rho1 / u1;
-h1 = A1 / w;
+h1 = cp*T1;
+height1 = A1 / w;
+
 
 
 %% Inlet
@@ -116,6 +118,7 @@ T3 = Trat*T(end);  p3 = prat*p(end);   rho3 = rhorat*rho(end);
 T03 = T3/Trat;     p03 = p3/prat;   rho03 = rho3/rhorat;
 a3 = sqrt(gamma*R*T3);
 u3 = M3*a3;
+h3 = cp*T3;
 
 p03/p01
 
@@ -126,18 +129,18 @@ x = 0:0.01:13;
 lower_wall_y = x*tand(theta);
 shock1_y = x*tand(B(1));
 
-plot(x,lower_wall_y,'k',x,h1*ones(1,length(x)),'k',x,shock1_y);
+plot(x,lower_wall_y,'k',x,height1*ones(1,length(x)),'k',x,shock1_y);
 hold on;
 %x*sind(B(1)) = h
-hit1_x = h1 / tand(B(1));
+hit1_x = height1 / tand(B(1));
 % shock 2 m = -tand(B(2)-theta)
 % shock 2 point : hit1_x, h
 % y - h = -tand(B(2)-theta)*(x-hit1_x)
-shock2_y = h1 - tand(B(2)-theta)*(x - hit1_x);
+shock2_y = height1 - tand(B(2)-theta)*(x - hit1_x);
 plot(x,shock2_y);
 
 % shock2_y = hit2_y = tand(theta)*hit2_x = h1 -tand(B(2)-theta)*(hit2_x - hit1_x)
-hit2_x = (h1+tand(B(2)-theta)*hit1_x) / (tand(theta) + tand(B(2)-theta));
+hit2_x = (height1+tand(B(2)-theta)*hit1_x) / (tand(theta) + tand(B(2)-theta));
 
 % shock 3 m = sind(B(3))
 % shock 3 point : hit2_x, sind(theta)*hit2_x
@@ -148,18 +151,18 @@ plot(x,shock3_y);
 
 
 % shock3_y = hit3_y = h1 = sind(theta)*hit2_x +sind(B(3))*(x - hit2_x)
-hit3_x = (h1-tand(theta)*hit2_x + tand(B(3))*hit2_x) / ...
+hit3_x = (height1-tand(theta)*hit2_x + tand(B(3))*hit2_x) / ...
     (tand(B(3)));
 
 % shock 4 m = -sind(B(4)-theta)
 % shock 4 point : hit3_x, h1
 % y - h1 = -sind(B(4)-theta)*(x-hit3_x)
-shock4_y = h1 -tand(B(4)-theta)*(x-hit3_x);
+shock4_y = height1 -tand(B(4)-theta)*(x-hit3_x);
 
 plot(x,shock4_y);
 
 % shock4_y = hit4_y = sind(theta)*hit4_x = h1 -sind(B(4))*(hit4_x-hit3_x);
-hit4_x = (h1+tand(B(4)-theta)*hit3_x) / (tand(theta) + tand(B(4)-theta));
+hit4_x = (height1+tand(B(4)-theta)*hit3_x) / (tand(theta) + tand(B(4)-theta));
 
 figure(2);
 hold on;
@@ -169,7 +172,7 @@ plot(x,x*tand(theta),'k');
 
 % upper wall
 x = hit1_x:0.01:13;
-plot(x,h1*ones(1,length(x)),'k');           
+plot(x,height1*ones(1,length(x)),'k');           
 
 % first shock
 x = 0:0.01:hit1_x;
@@ -178,7 +181,7 @@ plot(x,shock1_y);
 
 % second shock
 x = hit1_x:0.01:hit2_x;
-shock2_y = h1 -tand(B(2)-theta)*(x - hit1_x);
+shock2_y = height1 -tand(B(2)-theta)*(x - hit1_x);
 plot(x,shock2_y);
 
 % third shock
@@ -188,7 +191,7 @@ plot(x,shock3_y);
 
 % fourth shock
 x = hit3_x:0.01:hit4_x;
-shock4_y = h1 -tand(B(4)-theta)*(x-hit3_x);
+shock4_y = height1 -tand(B(4)-theta)*(x-hit3_x);
 plot(x,shock4_y);
 
 % end of lower wall
@@ -200,12 +203,14 @@ axis([0 13 0 4]);
 axis equal;
 
 
-h3 = h1 - (tand(theta)*hit4_x);
-A3 = h3*w;
+height3 = height1 - (tand(theta)*hit4_x);
+A3 = height3*w;
+
+
 
 %% DIFFUSOR
 A4 = A1;
-rho3*M3*sqrt(gamma*T3*R)*w*h3
+rho3*M3*sqrt(gamma*T3*R)*A3
 
 
 
